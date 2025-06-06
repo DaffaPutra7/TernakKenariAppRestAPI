2e23be51ae2d00363a0e9beb86c8fed829d693bf
DaffaPutra7 <putradika3604@gmail.com>
2025-06-06 18:30:55 +0700

    Menambahkan LoginBloc untuk manajemen state login (_onLoginRequested)

--- lib/presentation/auth/bloc/login/login_bloc.dart
+++ lib/presentation/auth/bloc/login/login_bloc.dart
@@ -2,14 +2,29 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:pamlanjut_restapi/data/model/request/auth/login_request_model.dart';
 import 'package:pamlanjut_restapi/data/model/response/auth_response_model.dart';
+import 'package:pamlanjut_restapi/data/repository/auth_repository.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial()) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final AuthRepository authRepository;
+
+  LoginBloc({required this.authRepository}) : super(LoginInitial()) {
+    on<LoginRequested>(_onLoginRequested);
+  }
+
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<LoginState> emit,
+  ) async {
+    emit(LoginLoading());
+
+    final result = await authRepository.login(event.requestModel);
+
+    result.fold(
+      (l) => emit(LoginFailure(error: l)),
+      (r) => emit(LoginSuccess(responseModel: r)),
+    );
   }
 }

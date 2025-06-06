29fecc9993fe39a5fde0899b442cf9e545d78256
DaffaPutra7 <putradika3604@gmail.com>
2025-06-06 23:38:18 +0700

    Menambahkan code untuk membuat fungsi logout di BuyerHomeScreen

--- lib/presentation/buyer/profile/home/buyer_home_screen.dart
+++ lib/presentation/buyer/profile/home/buyer_home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pamlanjut_restapi/presentation/bloc/get_all_burung_tersedia/get_all_burung_tersedia_bloc.dart';
+import 'package:pamlanjut_restapi/presentation/auth/login_screen.dart';
 
 class BuyerHomeScreen extends StatefulWidget {
   const BuyerHomeScreen({super.key});
@@ -8,8 +12,50 @@ class BuyerHomeScreen extends StatefulWidget {
 }
 
 class _BuyerHomeScreenState extends State<BuyerHomeScreen> {
+   @override
+  void initState() {
+    super.initState();
+    context.read<GetBurungTersediaBloc>().add(GetAllBurungTersediaEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.black),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return CupertinoAlertDialog(
+                    title: const Text('Konfirmasi'),
+                    content: const Text('Apakah anda yakin ingin keluar?'),
+                    actions: [
+                      CupertinoDialogAction(
+                        child: const Text('Batal'),
+                        onPressed: () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                            (route) => false,
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }

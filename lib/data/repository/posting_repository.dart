074e59d241fc9a43cf5cc92e7ca606f7f096de43
DaffaPutra7 <putradika3604@gmail.com>
2025-06-06 15:38:01 +0700

    Menambahkan repositori untuk PostingRepository dan fungsi addPostBurung

---
+++ lib/data/repository/posting_repository.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dartz/dartz.dart';
+import 'package:pamlanjut_restapi/data/model/request/admin/posting_jual_request_model.dart';
+import 'package:pamlanjut_restapi/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:pamlanjut_restapi/service/service_http_client.dart';
+
+class PostingRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  PostingRepository(this._serviceHttpClient);
+
+  Future<Either<String, BurungSemuaTersediabyIdModel>> addPostBurung(
+    PostingJualRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "admin/posting-jual",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 201) {
+        final profileResponse = BurungSemuaTersediabyIdModel.fromJson(jsonResponse);
+        log("Add Burung successful: ${profileResponse.message}");
+        return Right(profileResponse);
+      } else {
+        log("Add Burung failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Add burung failed");
+      }
+    } catch (e) {
+      log("Error in Add burung : $e");
+      return Left("An error occured while post burung: $e");
+    }
+  }
+}

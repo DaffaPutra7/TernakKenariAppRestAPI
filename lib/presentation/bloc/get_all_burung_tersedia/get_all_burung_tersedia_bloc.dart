4512952e1a1657a2be9403b25b636f47c002c487
DaffaPutra7 <putradika3604@gmail.com>
2025-06-06 23:28:14 +0700

    Menambahkan GetBurungTersediaBloc untuk manajemen data burung tersedia (_getAllBurungTersedia)

--- lib/presentation/bloc/get_all_burung_tersedia/get_all_burung_tersedia_bloc.dart
+++ lib/presentation/bloc/get_all_burung_tersedia/get_all_burung_tersedia_bloc.dart
@@ -1,14 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:pamlanjut_restapi/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:pamlanjut_restapi/data/repository/get_all_burung_tersedia_repository.dart';
 
 part 'get_all_burung_tersedia_event.dart';
 part 'get_all_burung_tersedia_state.dart';
 
-class GetAllBurungTersediaBloc extends Bloc<GetAllBurungTersediaEvent, GetAllBurungTersediaState> {
-  GetAllBurungTersediaBloc() : super(GetAllBurungTersediaInitial()) {
-    on<GetAllBurungTersediaEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+class GetBurungTersediaBloc extends Bloc<GetBurungTersediaEvent, GetBurungTersediaState> {
+  final GetAllBurungTersediaRepository getAllBurungTersediaRepository;
+
+  GetBurungTersediaBloc(this.getAllBurungTersediaRepository) : super(GetBurungTersediaInitial()) {
+    on<GetAllBurungTersediaEvent>(_getAllBurungTersedia);
+  }
+
+  Future<void> _getAllBurungTersedia(
+    GetAllBurungTersediaEvent event,
+    Emitter<GetBurungTersediaState> emit,
+  ) async {
+    emit(GetBurungTersediaLoading());
+
+    final result = await getAllBurungTersediaRepository.getAllBurungTersedia();
+
+    result.fold(
+      (error) => emit(GetBurungTersediaError(message: error)),
+      (burungTersedia) => emit(GetBurungTersediaLoaded(burungTersedia: burungTersedia)),
+    );
   }
 }

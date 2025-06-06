6d8b1e801a6ed79e6f16f954c67ea9d740e61a21
DaffaPutra7 <putradika3604@gmail.com>
2025-06-06 22:55:35 +0700

    Menambah code untuk tampilan ProfileViewBuyer

--- lib/presentation/buyer/profile/widget/profile_view_buyer.dart
+++ lib/presentation/buyer/profile/widget/profile_view_buyer.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:pamlanjut_restapi/core/core/core.dart';
+import 'package:pamlanjut_restapi/data/model/response/buyer/buyer_profile_response_model.dart';
 
 class ProfileViewBuyer extends StatelessWidget {
-  const ProfileViewBuyer({super.key});
+  final Data profile;
+  const ProfileViewBuyer({
+    super.key,
+    required this.profile
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Nama: ${profile.name}',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.05,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Alamat: ${profile.address}',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.04,
+              ),
+            ),
+            Text(
+              'No HP: ${profile.phone}',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.04,
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {
+                context.pushAndRemoveUntil(
+                  const BuyerHomeScreen(),
+                  (route) => false,
+                );
+              },
+              child: const Text('Go to Home'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

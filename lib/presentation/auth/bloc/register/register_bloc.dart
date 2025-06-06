2a63cd1be230369f3fc48d9ba487c258ac422dd5
DaffaPutra7 <putradika3604@gmail.com>
2025-06-06 18:38:47 +0700

    Menambahkan event RegisterRequested dalam RegisterEvent

--- lib/presentation/auth/bloc/register/register_bloc.dart
+++ lib/presentation/auth/bloc/register/register_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:pamlanjut_restapi/data/model/request/auth/register_request_model.dart';
 
 part 'register_event.dart';
 part 'register_state.dart';

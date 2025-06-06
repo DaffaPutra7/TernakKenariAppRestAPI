60560c3452647b6f1276f6712100825acd7796ec
DaffaPutra7 <putradika3604@gmail.com>
2025-06-06 23:08:26 +0700

    Menambahkan halaman manajemen profil pembeli (BuyerProfileScreen)

--- lib/presentation/buyer/profile/buyer_profile_screen.dart
+++ lib/presentation/buyer/profile/buyer_profile_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pamlanjut_restapi/presentation/buyer/profile/bloc/profile_buyer_bloc.dart';
+import 'package:pamlanjut_restapi/presentation/buyer/profile/widget/profile_buyer_form.dart';
+import 'package:pamlanjut_restapi/presentation/buyer/profile/widget/profile_view_buyer.dart';
 
 class BuyerProfileScreen extends StatefulWidget {
   const BuyerProfileScreen({super.key});
@@ -9,7 +13,44 @@ class BuyerProfileScreen extends StatefulWidget {
 
 class _BuyerProfileScreenState extends State<BuyerProfileScreen> {
   @override
+  initState() {
+    super.initState();
+    // Ambil profil pembeli saat halaman dimuat
+    context.read<ProfileBuyerBloc>().add(GetProfileBuyerEvent());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Profile Pembeli')),
+      body: BlocListener<ProfileBuyerBloc, ProfileBuyerState>(
+        listener: (context, state) {
+          if (state is ProfileBuyerAdded) {
+
+            // Refresh profil setelah tambah
+            context.read<ProfileBuyerBloc>().add(GetProfileBuyerEvent());
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Profile berhasil ditambahkan')),
+            );
+          }
+        },
+        child: BlocBuilder<ProfileBuyerBloc, ProfileBuyerState>(
+          builder: (context, state) {
+            if (state is ProfileBuyerLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (state is ProfileBuyerLoaded &&
+                state.profile.data.name.isNotEmpty) {
+              final profile = state.profile.data;
+              return ProfileViewBuyer(profile: profile);
+            }
+
+            // Default ke form jika tidak ada data atau error
+            return ProfileBuyerInputForm();
+          },
+        ),
+      ),
+    );
   }
 }
